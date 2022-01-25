6419d37d526d243c0736140eb0c815abdfc7acc2
devincornell <devincornell@example.com>
2022-01-25 12:46:37 -0500

    reorganized code for main runner

--- make_art.py
+++ make_art.py
@@ -26,25 +26,51 @@ from torchvision import transforms
 from torchvision.transforms import functional as TF
 #from tqdm.notebook import tqdm
 import tqdm
+from itertools import product
 
 import vqgan_clip_zquantize
 import vqganclip
 #from vqgan_clip_zquantize import *
 
 
-def convert_to_name(text: str):
-    '''Convert text prompt to url name.
-    '''
-    text = text.translate(str.maketrans('', '', string.punctuation))
-    text = '_'.join(text.split())
-    text = text.replace('__', '_')
-    return text
+@dataclasses.dataclass
+class Prompt:
+    base_text: str
+    post_text: str
+    post_name: str
 
+    @property
+    def text(self):
+        return f'{self.base_text} {self.post_text}'
+
+    @property
+    def name(self):
+        text = self.text
+        text = text.translate(str.maketrans('', '', string.punctuation))
+        text = '_'.join(text.split())
+        text = text.replace('__', '_')
+        return f'{text}_{self.post_name}'
 
 
 if __name__ == '__main__':
 
-    prompts = [
+    display_freq = None
+    save_freq = 5
+    run_name = 'tester'
+    training_folder = f'images/training/training_{run_name}/'
+    final_results_folder = f'images/final/final_{run_name}/'
+
+    try:
+        os.mkdir(training_folder)
+    except:
+        pass
+    
+    try:
+        os.mkdir(final_results_folder)
+    except:
+        pass
+
+    prompt_texts = [
         'star wars',
         'lightsaber luke skywalker',
         'lightsaber on spaceship',
@@ -80,75 +106,42 @@ if __name__ == '__main__':
 
     post_prompts = [
         ('', ''),
-        ('flickr', ' from Flickr'),
-        ('deviantart', ' from Deviantart'),
-        ('artstation', ' from Artstation'),
-        ('vray', ' from vray'),
-        ('ghibli', ' in the style of Studio Ghibli'),
-        ('unreal', ', rendered by Unreal Engine'),
+        ('flickr', 'from Flickr'),
+        ('deviantart', 'from Deviantart'),
+        ('artstation', 'from Artstation'),
+        ('vray', 'from vray'),
+        ('ghibli', 'in the style of Studio Ghibli'),
+        ('unreal', 'rendered by Unreal Engine'),
     ]
-
-    #for prompt in path_prompts:
-    #    print(f'{prompt}: {convert_to_name(prompt)}')
-    #exit()
+    
     
 
-    from itertools import product
-    #full_paths = path_prompts.copy()
-    seeds = list(range(10))
+    # generate prompts
+    prompts = [Prompt(t, pt, pn) for (t, (pn, pt)) in product(prompt_texts, post_prompts)]
+    
+    # other params
+    seeds = list(range(5))
 
-    #full_paths = [(*p, 0) for p in full_paths]
-    #prompt_params = [(prompt, post, )]
 
-    #full_paths = [(f'{n}_{sn}_{seed}',f'{d}{sd}', seed) for (n,d),(sn,sd), seed in product(path_prompts, post_scripts, seeds)]
-    #print(full_paths[-1])
+    params = list(product(prompts, seeds))
+    print(f'running {len(params)} param combinations')
+
+
 
     # start the main loop
-    for prompt, (post_name, post_text), seed in product(prompts, post_prompts, seeds):
-
-        prompt_full = f'{prompt}{post_text}'
+    
+    for prompt, seed in tqdm.tqdm(params):
         
-        #args = argparse.Namespace(
-        #    prompts = [prompt_full],
-        #    
-        #    image_prompts=[],
-        #    noise_prompt_seeds=[],
-        #    noise_prompt_weights=[],
-        #    size=[600, 400],
-        #    init_image=None,
-        #    #init_image='start_images/snowy_mountain_forest.png',
-        #    init_weight=0.,
-        #    clip_model='ViT-B/32',
-        #    vqgan_config='vqgan_clip_zquantize/vqgan_imagenet_f16_1024.yaml',
-        #    vqgan_checkpoint='vqgan_clip_zquantize/vqgan_imagenet_f16_1024.ckpt',
-        #    step_size=0.05,
-        #    cutn=64,
-        #    cut_pow=1.,
-        #    seed=seed,
-        #)
+        base_name = f'{prompt.name}_{seed}'
+        print(f'{base_name=}')
 
-        display_freq = None
-        save_freq = 5
-        run_name = 'tester'
-        training_folder = f'images/training/training_{run_name}/'
-        final_results_folder = f'images/final/final_{run_name}/'
-        prompt_name = f'{convert_to_name(prompt)}_{post_name}_{seed}'
-        print(f'starting prompt: {prompt_full} ({prompt_name})')
-        
-        try:
-            os.mkdir(training_folder)
-        except:
-            pass
-        
-        try:
-            os.mkdir(final_results_folder)
-        except:
-            pass
 
-        #init_image='start_images/snowy_mountain_forest.png',
         trainer = vqganclip.VQGANCLIP(
-            text_prompts=[prompt_full],
-            size=[600, 400]
+            text_prompts=[prompt.text],
+            init_image='images/start_images/mountain_sunset_1.png',
+            size=[600, 400],
+            seed=seed,
+            step_size=0.01,
         )
 
         with tqdm.tqdm() as train_progress_bar:
@@ -161,16 +154,13 @@ if __name__ == '__main__':
                 # display output if needed
                 if display_freq is not None and (trainer.i % display_freq == 0):
                     losses_str = ', '.join(f'{loss.item():g}' for loss in trainer.lossAll)
-                    tqdm.tqdm.write(f'{prompt_name}: i={trainer.i}, loss={sum(trainer.lossAll).item():g}, losses={losses_str}')
+                    tqdm.tqdm.write(f'{base_name}: i={trainer.i}, loss={sum(trainer.lossAll).item():g}, losses={losses_str}')
                 
                 # save image if required
                 if save_freq is not None and (trainer.i % save_freq == 0):
-                    trainer.save_current_image(f'{training_folder}/{prompt_name}_training.png')
+                    trainer.save_current_image(f'{training_folder}/{base_name}_training.png')
                     
                 # check convergence
-                if trainer.is_converged():
-                    trainer.save_current_image(f'{final_results_folder}/{prompt_name}_final.png')
+                if trainer.is_converged(thresh=0.01, quit_after=5):
+                    trainer.save_current_image(f'{final_results_folder}/{base_name}_final.png')
                     break
-
-
-
