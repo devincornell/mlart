9273472ef4d37da57a9067221cfbbf6dfcb7d26d
devincornell <devincornell@example.com>
2022-01-25 09:38:07 -0500

    added namespace

--- make_art.py
+++ make_art.py
@@ -25,10 +25,13 @@ from torch.nn import functional as F
 from torchvision import transforms
 from torchvision.transforms import functional as TF
 from tqdm.notebook import tqdm
-from vqgan_clip_zquantize import *
+import vqgan_clip_zquantize
+#from vqgan_clip_zquantize import *
 
 
 def convert_to_name(text: str):
+    '''Convert text prompt to url name.
+    '''
     text = text.translate(str.maketrans('', '', string.punctuation))
     text = '_'.join(text.split())
     text = text.replace('__', '_')
@@ -40,32 +43,35 @@ if __name__ == '__main__':
     prompts = [
         'star wars',
         'lightsaber luke skywalker',
-        'space ship with aliens',
-        'desert sun',
-        'black sky with stars',
-        'black sky city',
+        'lightsaber on spaceship',
+        'scarlet witch',
+        'batman begins',
+        'batman wearing a cape',
+        'batman and robin',
+        'green lantern',
+        'superman strength',
+        'super hero wearing a cape',
+        'spiderman fighting',
+        'spiderman punching villian',
+        'spiderman swinging on web',
+        'incredible hulk',
+        'super powered lazer',
+        'yoda lightsaber',
         'captain america',
-        'flying in the clouds',
-        'airplane in the clouds',
-        'bird in the sky',
-        'birds flying through clouds in the sky',
+        'Captain America',
         'thor god of thunder',
         'thor hammer thunder',
         'loki god of mischief',
         'Thor Norse mythology',
         'Loki Norse Mythology',
-        
-        #'love hapiness',
-        #'peace love',
-        #'culture and algorithms',
-        #'Hogwarts School of Witchcraft and Wizardry',
-        #'Duke University',
-        #'Stanford University',
-        #'Harvard University',
-        #'Marvel Comics',
-        #'Spiderman comics',
-        #'Spiderman cartoons',
-        #'Captain America',
+        #'black sky with stars',
+        #'black sky city',
+        #'space ship with aliens',
+        #'desert sun',
+        #'flying in the clouds',
+        #'airplane in the clouds',
+        #'bird in the sky',
+        #'birds flying through clouds in the sky',
     ]
 
     post_prompts = [
@@ -81,12 +87,13 @@ if __name__ == '__main__':
     #for prompt in path_prompts:
     #    print(f'{prompt}: {convert_to_name(prompt)}')
     #exit()
-    folder = f'heroes'
-    final_results_folder = f'heroes_final/'
+    run_name = 'heroes'
+    folder = f'images/intermediate_images/{run_name}/'
+    final_results_folder = f'images/final_images/final_{run_name}/'
 
     from itertools import product
     #full_paths = path_prompts.copy()
-    seeds = list(range(5))
+    seeds = list(range(10))
 
     #full_paths = [(*p, 0) for p in full_paths]
     #prompt_params = [(prompt, post, )]
@@ -119,9 +126,9 @@ if __name__ == '__main__':
             image_prompts=[],
             noise_prompt_seeds=[],
             noise_prompt_weights=[],
-            size=[400, 600],
-            #init_image=None,
-            init_image='start_images/snowy_mountain_forest.png',
+            size=[600, 400],
+            init_image=None,
+            #init_image='start_images/snowy_mountain_forest.png',
             init_weight=0.,
             clip_model='ViT-B/32',
             vqgan_config='vqgan_clip_zquantize/vqgan_imagenet_f16_1024.yaml',
@@ -135,13 +142,13 @@ if __name__ == '__main__':
         device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
         print('Using device:', device)
 
-        model = load_vqgan_model(args.vqgan_config, args.vqgan_checkpoint).to(device)
-        perceptor = clip.load(args.clip_model, jit=False)[0].eval().requires_grad_(False).to(device)
+        model = vqgan_clip_zquantize.load_vqgan_model(args.vqgan_config, args.vqgan_checkpoint).to(device)
+        perceptor = vqgan_clip_zquantize.clip.load(args.clip_model, jit=False)[0].eval().requires_grad_(False).to(device)
 
         cut_size = perceptor.visual.input_resolution
         e_dim = model.quantize.e_dim
         f = 2**(model.decoder.num_resolutions - 1)
-        make_cutouts = MakeCutouts(cut_size, args.cutn, cut_pow=args.cut_pow)
+        make_cutouts = vqgan_clip_zquantize.MakeCutouts(cut_size, args.cutn, cut_pow=args.cut_pow)
         n_toks = model.quantize.n_e
         toksX, toksY = args.size[0] // f, args.size[1] // f
         sideX, sideY = toksX * f, toksY * f
@@ -152,7 +159,7 @@ if __name__ == '__main__':
             torch.manual_seed(args.seed)
 
         if args.init_image:
-            pil_image = Image.open(fetch(args.init_image)).convert('RGB')
+            pil_image = Image.open(vqgan_clip_zquantize.fetch(args.init_image)).convert('RGB')
             pil_image = pil_image.resize((sideX, sideY), Image.LANCZOS)
             z, *_ = model.encode(TF.to_tensor(pil_image).to(device).unsqueeze(0) * 2 - 1)
         else:
@@ -169,25 +176,25 @@ if __name__ == '__main__':
         pMs = []
 
         for prompt in args.prompts:
-            txt, weight, stop = parse_prompt(prompt)
-            embed = perceptor.encode_text(clip.tokenize(txt).to(device)).float()
-            pMs.append(Prompt(embed, weight, stop).to(device))
+            txt, weight, stop = vqgan_clip_zquantize.parse_prompt(prompt)
+            embed = perceptor.encode_text(vqgan_clip_zquantize.clip.tokenize(txt).to(device)).float()
+            pMs.append(vqgan_clip_zquantize.Prompt(embed, weight, stop).to(device))
 
         for prompt in args.image_prompts:
-            path, weight, stop = parse_prompt(prompt)
-            img = resize_image(Image.open(fetch(path)).convert('RGB'), (sideX, sideY))
+            path, weight, stop = vqgan_clip_zquantize.parse_prompt(prompt)
+            img = vqgan_clip_zquantize.resize_image(Image.open(vqgan_clip_zquantize.fetch(path)).convert('RGB'), (sideX, sideY))
             batch = make_cutouts(TF.to_tensor(img).unsqueeze(0).to(device))
             embed = perceptor.encode_image(normalize(batch)).float()
-            pMs.append(Prompt(embed, weight, stop).to(device))
+            pMs.append(vqgan_clip_zquantize.Prompt(embed, weight, stop).to(device))
 
         for seed, weight in zip(args.noise_prompt_seeds, args.noise_prompt_weights):
             gen = torch.Generator().manual_seed(seed)
             embed = torch.empty([1, perceptor.visual.output_dim]).normal_(generator=gen)
-            pMs.append(Prompt(embed, weight).to(device))
+            pMs.append(vqgan_clip_zquantize.Prompt(embed, weight).to(device))
 
         def synth(z):
-            z_q = vector_quantize(z.movedim(1, 3), model.quantize.embedding.weight).movedim(3, 1)
-            return clamp_with_grad(model.decode(z_q).add(1).div(2), 0, 1)
+            z_q = vqgan_clip_zquantize.vector_quantize(z.movedim(1, 3), model.quantize.embedding.weight).movedim(3, 1)
+            return vqgan_clip_zquantize.clamp_with_grad(model.decode(z_q).add(1).div(2), 0, 1)
 
         @torch.no_grad()
         def checkin(i, losses, folder):
@@ -219,8 +226,8 @@ if __name__ == '__main__':
             z_max: float
             i: int = 0
             prev_losses: Any = dataclasses.field(default_factory=list)
-            display_freq: int = 1
-            save_freq: int = 10
+            display_freq: int = 100
+            save_freq: int = 100
 
             def train(self):
                 self.opt.zero_grad()
