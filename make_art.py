de4946ce851687ec2f011a553d69be170d80af56
devincornell <devincornell@example.com>
2022-01-26 16:32:16 -0500

    made some param updates

--- make_art.py
+++ make_art.py
@@ -12,6 +12,7 @@ from pathlib import Path
 import sys
 from typing import Any
 import string
+from itertools import product
 
 sys.path.append('./taming-transformers')
 
@@ -26,7 +27,7 @@ from torchvision import transforms
 from torchvision.transforms import functional as TF
 #from tqdm.notebook import tqdm
 import tqdm
-from itertools import product
+import imageio
 
 import vqgan_clip_zquantize
 import vqganclip
@@ -53,42 +54,34 @@ class Prompt:
 
 
 if __name__ == '__main__':
-
+    run_name = 'varun6'
+    max_iter = 150
     display_freq = None
     save_freq = 5
-    run_name = 'freedom4'
     image_path = pathlib.Path(f'images')
-    final_folder = image_path.joinpath(f'final/final_{run_name}/')
-    training_folder = image_path.joinpath(f'training/training_{run_name}/')
-    gif_folder = image_path.joinpath(f'gifs/gifs_{run_name}/')
+    final_folder = image_path.joinpath(f'final/{run_name}/')
+    training_folder = image_path.joinpath(f'training/{run_name}/')
+    gif_folder = image_path.joinpath(f'tmp/{run_name}/')
 
     final_folder.mkdir(parents=True, exist_ok=True)
     training_folder.mkdir(parents=True, exist_ok=True)
     gif_folder.mkdir(parents=True, exist_ok=True)
 
     prompt_texts = [
-        #'freedom',
-        #'american flag freedom',
-        #'struggle for freedom',
-        'power and freedom',
-        'power of freedom',
-        'freedom from power',
-        'freedom is beautiful',
-        #'mountain scene at sunset',
-        #'sunset',
-        #'sunset view from a mountain'
-        #'sunset mountain scene',
-        #'beautiful sunset',
+        'trancendance',
+        'trancendance monk',
+        'buddhist monk',
+        'rainfall',
     ]
 
     post_prompts = [
         ('', ''),
-        ('flickr', 'from Flickr'),
-        ('deviantart', 'from Deviantart'),
-        ('artstation', 'from Artstation'),
-        ('vray', 'from vray'),
-        ('ghibli', 'in the style of Studio Ghibli'),
-        ('unreal', 'rendered by Unreal Engine'),
+        #('flickr', 'from Flickr'),
+        #('deviantart', 'from Deviantart'),
+        #('artstation', 'from Artstation'),
+        #('vray', 'from vray'),
+        #('ghibli', 'in the style of Studio Ghibli'),
+        #('unreal', 'rendered by Unreal Engine'),
     ]
 
     # generate prompts
@@ -97,20 +90,21 @@ if __name__ == '__main__':
     # other params
     seeds = list(range(3))
     step_sizes = [0.05, 0.025, 0.01]
-    threshes = [0.01, 0.03, 0.05]
-    params = list(product(step_sizes, threshes, seeds, prompts))
+    params = list(product(seeds, step_sizes, all_prompts))
     print(f'running {len(params)} param combinations')
 
     # start the outer loop
-    for step_size, thresh, seed, prompt in tqdm.tqdm(params):
+    for seed, step_size, prompt in tqdm.tqdm(params):
         
-        base_name = f'{prompt.name}_step{step_size}_thresh{thresh}_{seed}'
+        base_name = f'{prompt.name}_step{step_size}_{seed}'
         print(f'{base_name=}')
 
         trainer = vqganclip.VQGANCLIP(
-            text_prompts=[prompt.text],
-            init_image='images/start_images/kiersten_freedom_painting_cropped.jpeg',
+            init_image='images/start_images/varun_painting_1.jpeg',
             size=[400, 600],
+            text_prompts=[prompt.text],
+            image_prompts=['images/start_images/starry_night.png'],
+            
             seed=seed,
             step_size=step_size,
             #device_name='cpu',
@@ -118,23 +112,33 @@ if __name__ == '__main__':
 
         with tqdm.tqdm() as train_progress_bar:
             while True:
-
+                
                 # display output if needed
                 if display_freq is not None and (trainer.i % display_freq == 0):
                     losses_str = ', '.join(f'{loss.item():g}' for loss in trainer.lossAll)
                     tqdm.tqdm.write(f'{base_name}: i={trainer.i}, loss={sum(trainer.lossAll).item():g}, losses={losses_str}')
+
+                # make subfolder for storing each iteration
+                subfolder = gif_folder.joinpath(f'{base_name}/')
+                subfolder.mkdir(parents=True, exist_ok=True)
                 
                 # save image if required
                 if save_freq is not None and (trainer.i % save_freq == 0):
                     trainer.save_current_image(training_folder.joinpath(f'{base_name}_training.png'))
-                    trainer.save_current_image(gif_folder.joinpath(f'{base_name}_iter{trainer.i:04d}.png'))
+                    trainer.save_current_image(subfolder.joinpath(f'{base_name}_iter{trainer.i:05d}.png'))
 
                 # run epoch
                 trainer.epoch()
                     
                 # check convergence
-                if trainer.is_converged(thresh=thresh, quit_after=5):
+                if trainer.i > max_iter or trainer.is_converged(thresh=0.001, quit_after=10):
                     trainer.save_current_image(final_folder.joinpath(f'{base_name}_final.png'))
+                    
+                    # save a gif image
+                    images = [imageio.imread(fn) for fn in sorted(map(str, subfolder.glob('*.png')))]
+                    imageio.mimsave(final_folder.joinpath(f'{base_name}_final.gif'), images, duration=0.5)
+                    
+                    #convert -delay 100 -loop 0 images/gifs/gifs_sunset7_starrynight/bright_stars_in_the_sky__step0.05_0/*.png
                     break
 
                 # update progress
---
+++ make_gif.sh
@@ -0,0 +1,2 @@
+
+convert -delay 100 -loop 1 images/gifs/gifs_varun1/transcendence__*.png varun1__.gif
