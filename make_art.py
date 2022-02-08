6a4d72642b4f3f4ae59f375987b08aa305ee22de
devincornell <devincornell@example.com>
2022-02-08 07:41:34 -0500

    trying params for blog

--- make_art.py
+++ make_art.py
@@ -11,57 +11,50 @@ import gifmaker
 if __name__ == '__main__':
 
     imfp = pathlib.Path('resource_images')
+    
+    #kwargs = dict(
+    #    #init_image = imfp.joinpath('angela/angela_garden1.jpg'),
+    #)
+    #jhilams = imfp.joinpath('jhilam').glob('*.png')
+    #stocks = imfp.joinpath('stock_photos').glob('*.png')
+#
+    #all_params = list()
+    #for jfp, sfp in product(jhilams, stocks):
+    #    all_params.append(gifmaker.AnimateParams(
+    #        #size = size,
+    #        init_image = sfp,
+    #        img_prompts = {0: [jfp]},
+    #        txt_prompts = {},
+    #        **kwargs,
+    #    ))
 
-    all_params = [
-        gifmaker.AnimateParams(
-            size = [600, 400],
-            init_image = imfp.joinpath('stock_photos/nyc.png'),
-            img_prompts = {0: [imfp.joinpath('jhilam/jhilam01.png')]},
-            txt_prompts = {},
-        ),
-        gifmaker.AnimateParams(
-            size = [600, 400],
-            init_image = imfp.joinpath('stock_photos/nyc.png'),
-            img_prompts = {0: [imfp.joinpath('jhilam/jhilam02_cropped.png')]},
-            txt_prompts = {},
-        ),
-        gifmaker.AnimateParams(
-            size = [600, 400],
-            init_image = imfp.joinpath('stock_photos/nyc.png'),
-            img_prompts = {0: [imfp.joinpath('jhilam/jhilam03.png')]},
-            txt_prompts = {},
-        ),
-        gifmaker.AnimateParams(
-            size = [600, 400],
-            init_image = imfp.joinpath('stock_photos/nyc.png'),
-            img_prompts = {0: [imfp.joinpath('jhilam/jhilam04.png')]},
-            txt_prompts = {},
-        ),
-        gifmaker.AnimateParams(
-            size = [600, 400],
-            init_image = imfp.joinpath('stock_photos/nyc.png'),
-            img_prompts = {0: [imfp.joinpath('jhilam/jhilam04.png')]},
-            txt_prompts = {0: ['beautiful sunset in the city']},
-        ),
-        gifmaker.AnimateParams(
-            size = [600, 400],
-            init_image = imfp.joinpath('stock_photos/nyc.png'),
-            img_prompts = {0: [imfp.joinpath('jhilam/jhilam04.png')]},
-            txt_prompts = {0: ['colors of the city']},
-        ),
-        gifmaker.AnimateParams(
-            size = [600, 400],
-            init_image = imfp.joinpath('stock_photos/nyc.png'),
-            img_prompts = {0: [imfp.joinpath('jhilam/jhilam04.png')]},
-            txt_prompts = {0: ['beautiful colors in the city']},
-        ),
-        gifmaker.AnimateParams(
-            size = [600, 400],
-            init_image = imfp.joinpath('stock_photos/nyc.png'),
-            img_prompts = {0: [imfp.joinpath('jhilam/jhilam05.png')]},
-            txt_prompts = {},
-        ),
+
+    texts = [
+        'Angels Landing Artstation',
+        'Angels Landing Ghibli',
+        'Grand Canyon Artstation',
+        'Grand Canyon Ghibli',
+        'building at sunset Artstation',
+        'building at sunset Deviantart',
+        'building at sunset Ghibli',
+        'sunset in the city Artstation',
+        'sunset in the city Deviantart',
+        'sunset in the city Ghibli',
     ]
+    kwargs = dict(
+        size = [600, 400],
+        max_iter = 300,
+    )
+    all_params = list()
+    for seed, text in product(range(5), texts):
+        all_params.append(gifmaker.AnimateParams(
+            #size = size,
+            init_image = None,
+            img_prompts = {},
+            txt_prompts = {0: [text]},
+            seed = seed,
+            **kwargs,
+        ))
     
     # count params
     print(f'running {len(all_params)} param configurations')
@@ -69,10 +62,9 @@ if __name__ == '__main__':
     # start the outer loop
     for params in tqdm.tqdm(all_params):
         gifmaker.make_gif(
-            'jhilam01_cityscape', 
-            pathlib.Path(f'output/group04'), 
+            'stylized03', 
+            pathlib.Path(f'output/blog01'), 
             params,
-            seed = 0, 
             display_freq = None
         )
 
